1bc8c7348b45a7070b4a7352ed42a773d8737f58
Christopher Armoskus <carmoskus@gmail.com>
2017-06-10 22:13:10 -0700

    Early DESeq2 analysis; doesnt do PCs

--- rea.R
+++ rea.R
@@ -1,3 +1,70 @@
 # Main interface for running rea
 
-library(docopt)
+doc = 'Usage: rea.R <counts> <cols> [ -p <np> ]
+
+options:
+ -p <np>  Number of principle components'
+
+suppressPackageStartupMessages(library(docopt))
+opts = docopt(doc)
+
+print(opts)
+
+# Set parameters
+counts.file = opts$counts
+cols.file = opts$cols
+
+# Load data
+counts = as.matrix(read.table(counts.file, header=TRUE, sep="\t", row.names=1))
+col.info = read.table(cols.file, header=TRUE, row.names=1, sep="\t")
+
+# Start DESeq2
+suppressPackageStartupMessages(library("DESeq2"))
+
+analyze = function (np) {
+    print(paste0("Analyze ", np))
+    
+    se = SummarizedExperiment(counts)
+    coldata = colData(se)
+    coldata$Group = col.info$group
+
+    # Add covariates
+    form = "Group"
+    if (np > 0) {
+        for (i in 1:arg2) {
+            coldata[,paste0("Covar", i)] = sva[,i]
+            form = paste0(form, " + Covar", i)
+        }
+    }
+    
+    print(form)
+    
+    dds = DESeqDataSetFromMatrix(countData=counts, colData=coldata, design = reformulate(form))
+    dds = DESeq(dds, minReplicatesForReplace=Inf)
+    
+    print(resultsNames(dds))
+    
+    # Output results
+    res = results(dds, c('Group', 'a', 'b'), cooksCutoff=Inf)
+    res = res[order(res$pvalue),]
+    #write.csv(as.data.frame(res), file=paste0(subdir, name, "_res.csv"))
+
+    print(res)
+
+    all.pvals = res$pvalue
+    all.padj = p.adjust(all.pvals, method="BH")
+    print(paste0("0.05    ", sum(all.padj <= 0.05, na.rm=TRUE), "    ", sum(res$padj <= 0.05, na.rm=TRUE)))
+
+    # Output metadata
+    #write.table(sizeFactors(dds), file=paste0(subdir, name, "_sizes.txt"), sep="\t")
+
+    #mc = as.data.frame(mcols(dds))
+    #rownames(mc) = rownames(dds)
+    #write.table(mc, file=paste0(subdir, name, "_meta.txt"), sep="\t")
+}
+
+if (is.null(opts$p)) {
+    analyze(0)
+} else {
+    analyze(as.integer(opts$p))
+}
